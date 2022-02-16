9fb67c3e4b0a778026fd3f2e1a07227e62c16c17
李建生 <lijiansheng@apexsoft.com.cn>
2022-02-16 17:25:45 +0800

    初始化清风商城项目

---
+++ qingfengmall-common/src/main/java/com/qingfeng/common/vo/CommonResponse.java
@@ -0,0 +1,14 @@
+package com.qingfeng.common.vo;
+
+/**
+ * <h1>通用响应对象定义</h1>
+ * {
+ *         "code": 100,
+ *         "note": "成功",
+ *         "result": {}
+ * }
+ */
+
+public class CommonResponse {
+
+}
